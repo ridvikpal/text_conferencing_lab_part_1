c9c8e1e8f09bb6457b3b6948d9fb516aef88145f
Ridvik Pal <ridvik.pal@mail.utoronto.ca>
2024-03-11 14:44:23 -0400

    started server.c

---
+++ server.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+// Function to run when adding new client
+void add_new_client(){
+    
+
+}
+
+
+int main(){
+    // Get the user input
+
+    // Load the authentication file (a file of usernames and passwords)
+
+    // Setup the server
+
+    // Listen on the incoming port
+
+    // Accept incoming connections loop
+
+    // Free global memory on exit
+
+    return 0;
+}
